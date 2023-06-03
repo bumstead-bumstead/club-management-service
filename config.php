ed6d0f8bdbd2981a300f0f29d158c5ca08856b9c
Yohwan Lee <91039622+bumstead-bumstead@users.noreply.github.com>
2023-06-04 03:48:24 +0900

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-﻿<?
-    $host = "localhost";
-    $dbid = "db2018170837";                    // 자신의 DB ID
-    $dbpass = "dldyghks951@gmail.com";                  // 자신의 DB Password
-    $dbname = "db2018170837";                  // 해당 프로젝트에서 사용하는 DB명
-?>
